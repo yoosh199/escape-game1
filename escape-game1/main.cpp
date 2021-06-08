875cd356343096f25c5db03751aa6a99e7784d41
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2021-06-08 22:06:25 +0900

    벽 구현, 보여줄 위치 가운데로 옮김

--- escape-game1/main.cpp
+++ escape-game1/main.cpp
@@ -9,25 +9,29 @@
 #define SUBMIT 5
 #define EASY 6
 #define HARD 7
-#define EASYMAPROW 30
+#define OK 8
+#define NO 9
+#define EASYMAPROW 50
 #define EASYMAPCOLUMN 15
 
+
+
 char easyMap[EASYMAPCOLUMN][EASYMAPROW] = {
-	{"00000000000000000000000000000"},
-	{"00000000000000000000000000e00"},
-	{"00000000000100000000000000000"},
-	{"00000000000100000000000000000"},
-	{"00000000000100000000000000000"},
-	{"00000000000100000000000000000"},
-	{"00000000000100000100000000000"},
-	{"00000000000100s00100000000000"},
-	{"00000000000000000001111100000"},
-	{"00000000000000000000000100000"},
-	{"00000000000000000000000100000"},
-	{"00000000000000000000000100000"},
-	{"00000000000000000000000100000"},
-	{"00000000000000000000000000000"},
-	{"00000000000000000000000000000"}
+	{"1111111111111111111111111111111111111111111111111"},
+	{"1000000000000000000000000000000000000000000000e01"},
+	{"1000000000000000000000000000000100000000000000001"},
+	{"1000000000000000000000000000000100000000000000001"},
+	{"1000000000000000000000000000000100000000000000001"},
+	{"1000000000000000000000100010000100000000000000001"},
+	{"1000000000000000000001010000000100000100000000001"},
+	{"100000000000000000000000s000000100000100000000001"},
+	{"1000000000000000000000100010000000000001111100001"},
+	{"1000000000000000000000000000000000000000000100001"},
+	{"1000000000000000000000000000000000000000000100001"},
+	{"1000000000000000000000000000000000000000000100001"},
+	{"1000000000000000000000000000000000000000000100001"},
+	{"1000000000000000000000000000000000000000000000001"},
+	{"1111111111111111111111111111111111111111111111111"}
 };
 
 enum {
@@ -57,7 +61,7 @@ int keyInput();
 void cursorView();
 int game(int menu);
 void map(int set);
-
+int mapMove(int x,int y);
 
 int main() {
 
@@ -78,26 +82,28 @@ void setColor(int foreground, int background) {
 
 void gameTitle() {
 	cursorView();
-	printf("			----------------------------------------\n");
-	printf("			---------------------WW-----------------\n");
-	printf("			--------------------WKOX----------------\n");
-	printf("			------------------WK0xokW---------------\n");
-	printf("			----------------N0xooookW---------------\n");
-	printf("			--------------WKxoooooxXNNW-------------\n");
-	printf("			-------------WOoooooldKKkKW-------------\n");
-	printf("			-------------KdloddoldOkokXW------------\n");
-	printf("			-----------WW0doodxdoodoooxK------------\n");
-	printf("			-----------NOOkoooxkkxxooookN-----------\n");
-	printf("			----------WXxodoodxkOOkxdooxX-----------\n");
-	printf("			--------WW-WOooodkkkOK0OxooOW-----------\n");
-	printf("			------------WKxddxOKXXXOdxKW------------\n");
-	printf("			--------------WX00KNWNXKXW--------------\n");
-	printf("			---------------WWWWWWWWWW---------------\n");
-	printf("			----------------------------------------\n");
+	system("mode con cols=50 lines=30 | title escape");
+	
+	printf("-------------------------------------------------\n");
+	printf("--------------------------WW---------------------\n");
+	printf("-------------------------WKOX--------------------\n");
+	printf("-----------------------WK0xokW-------------------\n");
+	printf("---------------------N0xooookW-------------------\n");
+	printf("-------------------WKxoooooxXNNW-----------------\n");
+	printf("------------------WOoooooldKKkKW-----------------\n");
+	printf("------------------KdloddoldOkokXW----------------\n");
+	printf("----------------WW0doodxdoodoooxK----------------\n");
+	printf("----------------NOOkoooxkkxxooookN---------------\n");
+	printf("---------------WXxodoodxkOOkxdooxX---------------\n");
+	printf("-------------WW-WOooodkkkOK0OxooOW---------------\n");
+	printf("-----------------WKxddxOKXXXOdxKW----------------\n");
+	printf("-------------------WX00KNWNXKXW------------------\n");
+	printf("--------------------WWWWWWWWWW-------------------\n");
+	printf("-------------------------------------------------\n");
 }
 
 int gameMenu() {
-	int x = 42;
+	int x = 23;
 	int y = 18;
 	gotoxy(x-2, y);
 	printf("> 시작");
@@ -196,7 +202,7 @@ int game(int menu) {
 	
 	}
 	else if (menu == 1) {
-		int x = 42;
+		int x = 23;
 		int y = 18;
 		gotoxy(x-2, y);
 		printf("> 쉬움");
@@ -242,12 +248,109 @@ int game(int menu) {
 
 void map(int set) {
 	system("cls");
+	int start_x, start_y;
 	if (set == EASY) {
 		for (int i = 0; i < EASYMAPCOLUMN; i++) {
 			for (int j = 0; j < EASYMAPROW; j++) {
-				printf("%c", easyMap[i][j]);
+				if (easyMap[i][j] == 's') {
+					start_x = j;
+					start_y = i;
+				}
+			}
+		
+		}
+		int n = 0;
+		for (int k = start_y; k < start_y+3; k++) {
+			gotoxy(23, 5 + n);
+			for (int h = start_x; h < start_x+3; h++) {
+				printf("%c", easyMap[k - 1][h - 1]);
 			}
 			printf("\n");
+			n++;
+	
 		}
+		int key;
+		while (1) {
+			key = keyInput();
+			
+			if (key == UP) {
+				if (easyMap[start_y - 1][start_x] == '0') {
+					system("cls");
+					easyMap[start_y][start_x] = '0';
+					start_y -= 1;
+					easyMap[start_y][start_x] = 's';
+					n = 0;
+					for (int k = start_y; k < start_y + 3; k++) {
+						gotoxy(23, 5 + n);
+						for (int h = start_x; h < start_x + 3; h++) {
+							printf("%c", easyMap[k - 1][h - 1]);
+						}
+						n++;
+
+					}
+				}
+				else if (easyMap[start_y - 1][start_x] == 'e') {
+					//성공
+				}
+			}
+			else if (key == DOWN) {
+				if (easyMap[start_y + 1][start_x] == '0') {
+					system("cls");
+					easyMap[start_y][start_x] = '0';
+					start_y += 1;
+					easyMap[start_y][start_x] = 's';
+					n = 0;
+					for (int k = start_y; k < start_y + 3; k++) {
+						gotoxy(23, 5 + n);
+						for (int h = start_x; h < start_x + 3; h++) {
+							printf("%c", easyMap[k - 1][h - 1]);
+						}
+						n++;
+
+					}
+				}
+				
+
+			}
+			else if (key == LEFT) {
+				if (easyMap[start_y][start_x-1] == '0') {
+					system("cls");
+					easyMap[start_y][start_x] = '0';
+					start_x -= 1;
+					easyMap[start_y][start_x] = 's';
+					n = 0;
+					for (int k = start_y; k < start_y + 3; k++) {
+						gotoxy(23, 5 + n);
+						for (int h = start_x; h < start_x + 3; h++) {
+							printf("%c", easyMap[k - 1][h - 1]);
+						}
+						n++;
+
+					}
+				}
+				
+
+			}
+			else if (key == RIGHT) {
+				if (easyMap[start_y][start_x + 1] == '0') {
+					system("cls");
+					easyMap[start_y][start_x] = '0';
+					start_x += 1;
+					easyMap[start_y][start_x] = 's';
+					n = 0;
+					for (int k = start_y; k < start_y + 3; k++) {
+						gotoxy(23, 5 + n);
+						for (int h = start_x; h < start_x + 3; h++) {
+							printf("%c", easyMap[k - 1][h - 1]);
+						}
+						n++;
+
+					}
+				}
+				
+			}
+		}
+		
 	}
 }
+
